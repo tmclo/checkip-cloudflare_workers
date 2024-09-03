809cf56ce3738d37de315a266c46d4f41353b5eb
Tom McLoughlin <root@pinguin.uk>
2024-09-03 12:30:13 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Check IP with Cloudflare Workers
+
+This is a simple worker which fetches the 'cf-connecting-ip' header and returns the following:
+```
+Current IP address: ${ip}
+```
+
+Which is the correct format for a Check IP Service within pfSense's Dynamic DNS package.
+
+[Check it out!](https://as204291.net/checkip)
+
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request));
+})
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+async function handleRequest(request) {
+	const { cf } = request || {};
+	const ip = request.headers.get('cf-connecting-ip');
+
+	return new Response(`Current IP Address: ${ip}`, {
+		headers: { 'content-type': 'text/plain' },
+	});
+}
